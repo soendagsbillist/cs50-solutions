240c4587d5519d7a6e7d03ac1a96277ae15bbd23
soendagsbillist <vladislav.tyutin@me.com>
2020-10-04 19:57:55 +0200

    Add checking for the right amount of cla

---
+++ week_2/caesar.c
@@ -0,0 +1,19 @@
+/*
+  Implementation of the caesar cipher for cs50 pset2
+*/
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    if(argc == 2)
+    {
+	printf("Success\n");
+    }
+    else
+    {
+	printf("Usage: ./caesar key\n");
+	return 1;
+    }
+}
